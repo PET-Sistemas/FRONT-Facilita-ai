ce2fd19d83cbbbc12ec2cd4f92ca43501e2453b8
Caue Moraes <cauemor@gmail.com>
2025-11-20 20:51:43 -0400

    deletando feat de dados que está sem uso

--- src/features/dados/model/Dados.js
+++
@@ -1,9 +0,0 @@
-export default class CadastroModel {
-    constructor(address, phone, email, name, newPicture) {
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-        this.name = name;
-        this.newPicture = newPicture;
-    }
-}
